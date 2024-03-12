b4a6778ea9bd1a02cc30174e9a07fcb82086d25c
jjdoria <72863049+jjdoria@users.noreply.github.com>
2024-03-12 19:29:25 +0100

    Método fábrica para ropa, comida, electronica

---
+++ Models/FabricaProducto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartTrade.Models
+{
+    internal class FabricaProducto
+    {
+        public Producto crearProducto(string tipo) {
+            Producto nuevoProducto = null;
+            if (tipo == "ropa")
+            {
+                nuevoProducto = new Ropa();
+            }
+            else if (tipo == "comida")
+            {
+                nuevoProducto = new Comida();
+            }
+            else if (tipo == "electronica") { 
+                nuevoProducto = new Electronica();
+            }
+            return nuevoProducto;
+        }
+    }
+}
---
+++ Models/Ropa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartTrade.Models
+{
+    internal class Ropa : Producto
+    {
+        public Ropa() { }
+    }
+}
