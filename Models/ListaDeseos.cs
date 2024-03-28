ac0ddf4978b9a4378288a3205ccd12494f738d95
Jeroni Altur Frasquet <JALTFRA@upv.edu.es>
2024-03-28 13:56:31 +0100

    listadeseos

---
+++ Models/ListaDeseos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartTrade.Models
+{
+    internal class ListaDeseos
+    {
+        public List<Producto> productos { get; set; }
+        public ListaDeseos() 
+        { 
+            productos = new List<Producto>();
+        }
+
+        public void añadirProducto(Producto p)
+        {
+            productos.Add(p);
+        }
+
+        public void eliminarProducto(Producto p)
+        {
+            productos.Remove(p);
+        }
+    }
+}
