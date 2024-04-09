842e054d54c4544fcdf8b83f6f67f1393d915cf0
Jeroni Altur <jeronietaltur@gmail.com>
2024-04-09 16:38:00 +0200

    a

--- Services/SmartTradeServices.cs
+++ Services/SmartTradeServices.cs
@@ -159,11 +159,6 @@ namespace SmartTrade.Services
 
         public void IniciarBD()
         {
-            bd.Crear<Producto>();
-            bd.Crear<Valoracion>();
-            bd.Crear<Comida>();
-            bd.Crear<Electronica>();
-            bd.Crear<Ropa>();
             bd.BorrarTodo();
 
             Producto p1 = new Producto("teclado", "teclado con pad numerico", 20, null, null, null);
