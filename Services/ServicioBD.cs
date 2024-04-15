3979a96aa2bc5589dffd74e94009a9591a3ddde2
Jeroni Altur <jeronietaltur@gmail.com>
2024-04-15 22:44:53 +0200

    servicio

--- Services/ServicioBD.cs
+++ Services/ServicioBD.cs
@@ -1,52 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Data;
 using SQLite;
 using SmartTrade.Models;
-using System.Linq.Expressions;
-using MySql.Data.MySqlClient;
 
 namespace SmartTrade.Services
 {
-    internal class ServicioBD : BD
+    internal class ServicioBD : IDisposable
     {
         private readonly SQLiteConnection _conexion;
 
-        public ServicioBD(SQLiteConnection conexion)
+        public ServicioBD(string dbPath)
         {
-            this._conexion = conexion;
+            _conexion = new SQLiteConnection(dbPath);
+            _conexion.CreateTable<Producto>();
+            _conexion.CreateTable<Valoracion>();
+            _conexion.CreateTable<Electronica>();
+            _conexion.CreateTable<Comida>();
+            _conexion.CreateTable<Ropa>();
         }
 
         public void Insertar<T>(T entity) where T : class
         {
             _conexion.Insert(entity);
-            _conexion.Commit();
         }
 
         public void Borrar<T>(T entity) where T : class
         {
             _conexion.Delete(entity);
-            _conexion.Commit();
         }
 
         public void Limpiar<T>() where T : class
         {
             _conexion.DeleteAll<T>();
-            _conexion.Commit();
         }
 
         public List<T> Todo<T>() where T : new()
         {
             return _conexion.Table<T>().ToList();
-        }
-
-        public void Crear<T>() where T : class
-        {
-            _conexion.CreateTable<T>();
-            _conexion.Commit();
         }
 
         public List<T> TodoOrdenado<T, U>(string orderByColumn) where T : new()
@@ -60,26 +52,23 @@ namespace SmartTrade.Services
             Limpiar<Comida>();
             Limpiar<Ropa>();
             Limpiar<Electronica>();
+            Limpiar<Valoracion>();
         }
+
         public T BuscarPorID<T>(int id) where T : class, new()
         {
             return _conexion.Find<T>(id);
         }
+
         public void Actualizar<T>(T entity) where T : class
         {
             _conexion.Update(entity);
-            _conexion.Commit();
         }
 
-        public void CrearTablas()
+        public void Dispose()
         {
-            _conexion.CreateTable<Producto>();
-            _conexion.CreateTable<Valoracion>();
-            _conexion.CreateTable<Electronica>();
-            _conexion.CreateTable<Comida>();
-            _conexion.CreateTable<Ropa>();
-
-            _conexion.Commit();
+            _conexion.Close();
+            _conexion.Dispose();
         }
     }
 }
